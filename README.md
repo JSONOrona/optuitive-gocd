ecee3271538ed80d4e607e0d1e0f5859ea3d63a1
Jason Orona <jason.orona@theaiteam.coml>
2017-01-16 10:33:29 -0600

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# optuitive-gocd
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: optuitive-gocd
+# Recipe:: default
+#
+# Copyright (c) 2017 The Authors, All Rights Reserved.
